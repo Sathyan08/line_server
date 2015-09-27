f139b47c52b9c3c709a995a735834ffa87f4dcc1
Sathyan Mathai <Sathyan.Mathai@gmail.com>
2015-09-27 14:39:43 -0400

    updated the seed file so that it canload the text and scrub out the newline characters.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,10 +5,10 @@ line_file = File.join(current_directory, line_file_name)
 
 lines = File.readlines(line_file)
 
-lines.each_with_index do |line, index|
-
-  puts line
-
+lines.each_with_index do |line_text, index|
+  line_number = index + 1
+  clean_text = line_text.strip.gsub(/\n/, '')
+  new_line = Line.create(line_number: line_number, line_text: clean_text)
 end
 
 
