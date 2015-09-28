4be630ceb21d2cb3d785a9e978739ce6fed44ca3
Sathyan Mathai <Sathyan.Mathai@gmail.com>
2015-09-27 22:26:48 -0400

    Started Readme

--- app/models/line.rb
+++ app/models/line.rb
@@ -1,7 +1,6 @@
 class Line < ActiveRecord::Base
 
   validates :line_number, presence: true, uniqueness: true
-  validates :line_text, presence: true
 
   def self.find_by_redis(key)
     $redis.get(key)
--- db/migrate/20150927175624_create_lines.rb
+++ db/migrate/20150927175624_create_lines.rb
@@ -2,7 +2,7 @@ class CreateLines < ActiveRecord::Migration
   def change
     create_table :lines do |t|
       t.integer :line_number, null: false
-      t.string  :line_text, null: false
+      t.string  :line_text
 
       t.timestamps null: false
     end
