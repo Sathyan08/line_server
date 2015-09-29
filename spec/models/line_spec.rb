d5079298ccee05107d3a77d3b827194a621d1aa9
Sathyan Mathai <Sathyan.Mathai@gmail.com>
2015-09-28 20:33:21 -0400

    Added test to see if line numbers must be unique.

--- spec/models/line_spec.rb
+++ spec/models/line_spec.rb
@@ -4,12 +4,16 @@ RSpec.describe Line, type: :model do
   let(:line) { create(:line) }
 
   it "Has an line number that is an integer." do
-    line = create(:line)
     expect(line.line_number.is_a?(Integer)).to be true
   end
 
   it "Has text that is an string." do
-    line = create(:line)
     expect(line.line_text.is_a?(String)).to be true
   end
+
+  it "Cannot save if it has the same line number as another line." do
+    second_line = line.dup
+    expect(second_line.save).to be false
+  end
+
 end
